b6bae2ef9af39780c7f0db7abe465980b2ab9278
colewortman <colewortman@gmail.com>
2025-06-01 13:22:19 -0500

    Fixed route conflicts

--- eventp-api/routes/userRoutes.js
+++ eventp-api/routes/userRoutes.js
@@ -4,7 +4,7 @@ const UserDetailService = require('../service/UserDetailService');
 
 router.get('/', UserDetailService.getAllUsers);
 router.get('/:id', UserDetailService.getUserById);
-router.get('/:username', UserDetailService.getUserByUsername);
+router.get('/username/:username', UserDetailService.getUserByUsername);
 router.post('/', UserDetailService.createUser);
 router.put('/:id', UserDetailService.updateUser);
 router.delete('/:id', UserDetailService.deleteUser);
