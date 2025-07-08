ade3607869920e2701c18a679b13cf276f92a900
colewortman <colewortman@gmail.com>
2025-07-08 14:55:10 -0500

    Fixing test error

--- src/components/UserDetailSignUp.tsx
+++ src/components/UserDetailSignUp.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { createUserDetail } from '../services/userService';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './UserDetailSign.module.css';
 import BlurText from './BlurText';
 
