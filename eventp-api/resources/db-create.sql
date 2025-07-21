4835d7c77761485a6a3f20d8179caa83370c9e4f
colewortman <colewortman@gmail.com>
2025-07-21 00:30:29 -0500

    Updated DB, restrict to future events and delete eventuser with event

--- eventp-api/resources/db-create.sql
+++ eventp-api/resources/db-create.sql
@@ -2,7 +2,7 @@
 CREATE SCHEMA IF NOT EXISTS eventp;
 
 CREATE TABLE eventp.user_detail (
-    user_detail_id SERIAL NOT NULL,
+    user_detail_id INT NOT NULL,
     user_detail_username VARCHAR(255) NOT NULL,
     user_detail_password VARCHAR(255) NOT NULL,
     user_detail_email VARCHAR(255) NOT NULL,
@@ -12,11 +12,13 @@ CREATE TABLE eventp.user_detail (
 );
 
 CREATE TABLE eventp.event_detail (
-    event_detail_id SERIAL NOT NULL,
+    event_detail_id INT NOT NULL,
     event_detail_created_by INT NOT NULL,
     event_detail_name VARCHAR(255) NOT NULL,
     event_detail_description TEXT,
-    event_detail_date DATE NOT NULL,
+    event_detail_date DATE NOT NULL CHECK (
+        event_detail_date >= CURRENT_DATE
+    ),
     event_detail_time TIME NOT NULL,
     event_detail_location VARCHAR(255),
     event_detail_capacity INT NOT NULL,
@@ -25,10 +27,12 @@ CREATE TABLE eventp.event_detail (
 );
 
 CREATE TABLE eventp.eventuser (
-    event_detail_id SERIAL NOT NULL,
-    user_detail_id SERIAL NOT NULL,
+    event_detail_id INT NOT NULL,
+    user_detail_id INT NOT NULL,
     CONSTRAINT pk_eventuser PRIMARY KEY (
         event_detail_id,
         user_detail_id
-    )
+    ),
+    CONSTRAINT fk_eventuser_event FOREIGN KEY (event_detail_id) REFERENCES eventp.event_detail (event_detail_id) ON DELETE CASCADE,
+    CONSTRAINT fk_eventuser_user FOREIGN KEY (user_detail_id) REFERENCES eventp.user_detail (user_detail_id) ON DELETE CASCADE
 );
